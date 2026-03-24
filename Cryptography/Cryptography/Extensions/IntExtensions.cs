ec8203a015e425a702cfe5074a1ce052a21fa186
User <kir228005@yandex.ru>
2026-03-24 20:49:26 +0300

    lab2

--- Cryptography/Cryptography/Extensions/IntExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Cryptography.Extensions
-{
-    public static class IntExtensions
-    {
-        public static bool IsOdd(this int number) => (number & 1) == 1;
-        public static bool IsEven(this int number) => (number & 1) == 0;
-    }
-}
